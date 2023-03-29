1d12c90f49578a0cabfb0958cbe244739403fc1f
Warlla0101 <104159596+Warlla0101@users.noreply.github.com>
2023-03-29 17:58:57 -0300

    Add files via upload

---
+++ CBBAtividadePratica2.java
@@ -0,0 +1,14 @@
+public class CBBAtividadePratica2 {
+    public static void main(String[] args) {
+
+        int var1 = 150;
+        int var2 = 80;
+        int var3 = 70;
+
+        int soma = var1 + var2 + var3;
+        System.out.println("Soma: " + soma);
+
+        double media = (double) soma / 3;
+        System.out.println("Media: " + media);
+    }
+}
