9982e297e3b570d3af76cfa1775f233ec5f5ef25
Warlla0101 <104159596+Warlla0101@users.noreply.github.com>
2023-03-29 18:01:45 -0300

    Add files via upload

---
+++ CBBAtividadePratica3.java
@@ -0,0 +1,10 @@
+public class CBBAtividadePratica3 {
+    public static void main(String[] args) {
+
+        double altura = 3;
+        double base = 5;
+
+        double area = altura * base;
+        System.out.println("Area: " + area);
+    }
+}
